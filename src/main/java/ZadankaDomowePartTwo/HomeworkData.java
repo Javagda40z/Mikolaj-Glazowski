bb78efa66343bf60efc126a10de0252d647d4397
GM <m.glazowski06@gmail.com>
2020-04-19 00:00:35 +0200

    Sobota 18.04.2020 - Sprawdzenie przestępności roku

--- src/main/java/ZadankaDomowePartTwo/HomeworkData.java
+++ src/main/java/ZadankaDomowePartTwo/HomeworkData.java
@@ -12,6 +12,10 @@ public class HomeworkData {
         LocalDate samaData = LocalDate.now();
         LocalDate pierwszaData = LocalDate.of(2005,4,2);
         LocalDate drugaData = LocalDate.of(2001,9,11);
+        int rok1 = 1953;
+        int rok2 = 1684;
+        int rok3 = 1900;
+        int rok4 = 2020; //todo: change to Scanner
         int dni = 42; //TODO: change to Scanner
         System.out.println(dataCzas); //zadanie 1
         long ileDniMinelo = ChronoUnit.DAYS.between(LocalDate.of(2020,1,1), samaData);
@@ -24,6 +28,25 @@ public class HomeworkData {
         System.out.println(samaData.plus(Period.ofDays(dni)));// zadanie 6
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm, dd-MM-yyyy");
         System.out.println(dataCzas.format(formatter)); //zadanie 8
+
+        czyPrzestepny(rok1);
+        czyPrzestepny(rok2);
+        czyPrzestepny(rok3);
+        czyPrzestepny(rok4);
+
+    }
+
+    public static void czyPrzestepny (int rok){//zadanie 7.2
+        if (rok%4 != 0){
+            System.out.println("Nie jest przestępny");
+        } else if (rok % 100 == 0){
+            if(rok % 400 == 0){
+                System.out.println("Jest przestępny");
+            }else {
+                System.out.println("Nie jest przestępny");
+            }
+        } else System.out.println("Jest przestępny");
+
     }
 
 
--- src/main/java/ZadankaDomowePartTwo/Obiekty/Punkt.java
+++ src/main/java/ZadankaDomowePartTwo/Obiekty/Punkt.java
@@ -1,4 +1,10 @@
 package ZadankaDomowePartTwo.Obiekty;
 
+import java.util.Random;
+
 public class Punkt {
+
+    Random random = new Random();
+
+
 }
